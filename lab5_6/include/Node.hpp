0111e8799f7149104a21260a12709015aca6359a
KingaEwaAdamska <kingaewa05@gmail.com>
2025-11-17 15:20:44 +0100

    some cleaning

--- lab5_6/include/Node.hpp
+++ lab5_6/include/Node.hpp
@@ -11,96 +11,111 @@ struct Variable;
 class Tree;
 
 class Node {
-  private:
-    Node *parent;
-
   public:
-    Node(Node *parent = NULL); 
     virtual ~Node();
     virtual Node* getLeaf() = 0;
-    Node* getParent();
     virtual float getValue() = 0;
     virtual std::string toString() = 0;
-    
+
+    Node* getParent();
+
     static Node* createNode(const std::vector<std::string> *tokens, int &actToken, CmdStatus &status, std::vector<Variable*> *variables);
+
+    Node(Node *parent = NULL);
+
+  private:
+    Node *parent;
 };
 
 class OperatorNode : public Node {
     friend class Node;
-  protected:
-    std::vector<Node*> childs;
+
   public:
-    OperatorNode(Node *parent = NULL);
     ~OperatorNode();
     Node* getLeaf();
     void changeChild(Node *newChild);
-
     static void joinSubtreeRoot(Node *subtreeRoot, OperatorNode *actNode);
+
+    OperatorNode(Node *parent = NULL);
+
+  protected:
+    std::vector<Node*> childs;
+
 };
 
 class AdditionNode : public OperatorNode {
   public:
-    AdditionNode(Node *parent = NULL);
     float getValue();
     std::string toString();
+
+    AdditionNode(Node *parent = NULL);
 };
 
 class SubtractionNode : public OperatorNode {
   public:
-    SubtractionNode(Node *parent = NULL);
     float getValue();
     std::string toString();
+
+    SubtractionNode(Node *parent = NULL);
 };
 
 class MultiplicationNode : public OperatorNode {
   public:
-    MultiplicationNode(Node *parent = NULL);
     float getValue();
     std::string toString();
+
+    MultiplicationNode(Node *parent = NULL);
 };
 
 class DivisionNode : public OperatorNode {
   public:
-    DivisionNode(Node *parent = NULL);
     float getValue();
-    std::string toString(); 
+    std::string toString();
+
+    DivisionNode(Node *parent = NULL);
 };
 
 class SinusNode : public OperatorNode {
   public:
-    SinusNode(Node *parent = NULL);
     float getValue();
     std::string toString();
+
+    SinusNode(Node *parent = NULL);
 };
 
 class CosinusNode : public OperatorNode {
   public:
-    CosinusNode(Node *parent = NULL);
     float getValue();
     std::string toString();
+
+    CosinusNode(Node *parent = NULL);
 };
 
 class NumberNode : public Node {
-  private:
-    float value;
   public:
-    NumberNode(float val, Node *parent = NULL);
     float getValue();
     std::string toString();
     Node* getLeaf();
+
+    NumberNode(float val, Node *parent = NULL);
+
+  private:
+    float value;
 };
 
 class VarNode : public Node {
+  public:
+    float getValue();
+    std::string toString();
+    Node* getLeaf();
+    ~VarNode();
+
+    VarNode(std::string varName, std::vector<Variable*> *variables, Node *parent = NULL);
+
   private: 
     Variable* var;
     std::vector<Variable*> *variables;
     Variable* addVariable(std::string var, std::vector<Variable*> *variables);
-  public:
-    float getValue();
-    std::string toString();
-    VarNode(std::string varName, std::vector<Variable*> *variables, Node *parent = NULL);
-    ~VarNode();
-    Node* getLeaf();
 };
 
 #endif
